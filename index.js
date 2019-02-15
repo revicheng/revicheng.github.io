062c98bf45a255c9b7c0b47f0e129efa20bd8817
revi <revi.cheng@gmail.com>
2019-02-15 11:54:56 -0800

    rebase?

---
+++ index.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    navigation: true,
+    navigationPosition: 'right'
+});
