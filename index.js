f53b5610ab601ae8303c4181625caa57a6d53ffa
revi <revi.cheng@gmail.com>
2020-08-24 00:46:51 -0700

    remove is

--- index.js
+++ index.js
@@ -2,17 +2,3 @@ new TypeIt('#bio_text', {
     })
     .type("A software developer and an aspiring photographer ")
     .go();
-
-var elem = document.querySelector('.container');
-var infScroll = new InfiniteScroll( elem, {
-    // options
-    path: '#photos{{#}}',
-    append: '.post',
-    history: false,
-});
-
-// element argument can be a selector string
-// for an individual element
-var infScroll = new InfiniteScroll( '.container', {
-    // options
-});
